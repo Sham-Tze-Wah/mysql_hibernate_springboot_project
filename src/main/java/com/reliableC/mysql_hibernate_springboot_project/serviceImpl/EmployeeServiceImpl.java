99231d1f5cb9d21282d58c444f3470b77e0dad56
Sham-Tze-Wah <shamtzewah@gmail.com>
2022-12-01 18:05:42 +0800

    Creating Interface

--- src/main/java/com/reliableC/mysql_hibernate_springboot_project/serviceImpl/EmployeeServiceImpl.java
+++ src/main/java/com/reliableC/mysql_hibernate_springboot_project/serviceImpl/EmployeeServiceImpl.java
@@ -1,9 +1,14 @@
 package com.reliableC.mysql_hibernate_springboot_project.serviceImpl;
 
+import com.reliableC.mysql_hibernate_springboot_project.model.Employee;
 import com.reliableC.mysql_hibernate_springboot_project.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return null;
+    }
 }
