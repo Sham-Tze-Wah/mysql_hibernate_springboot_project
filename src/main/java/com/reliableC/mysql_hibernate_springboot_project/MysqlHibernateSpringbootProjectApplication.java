df80c59383d61dd1a0cc494cae7a6b0a527e5224
Sham-Tze-Wah <shamtzewah@gmail.com>
2022-12-01 17:01:22 +0800

    Initial commit

---
+++ src/main/java/com/reliableC/mysql_hibernate_springboot_project/MysqlHibernateSpringbootProjectApplication.java
@@ -0,0 +1,15 @@
+package com.reliableC.mysql_hibernate_springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlHibernateSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+
+		//This is the application
+		SpringApplication.run(MysqlHibernateSpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reliableC/mysql_hibernate_springboot_project/MysqlHibernateSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reliableC.mysql_hibernate_springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlHibernateSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
